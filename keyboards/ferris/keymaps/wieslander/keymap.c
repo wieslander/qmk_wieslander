8487afb45f5ff91926601eb286234a835b56f1b8
Pär Wieslander <par.wieslander@attentec.se>
2024-06-20 13:37:45 +0200

    Try a programmer's Dvorak inspired number row layout

--- keyboards/ferris/keymaps/wieslander/keymap.c
+++ keyboards/ferris/keymaps/wieslander/keymap.c
@@ -35,13 +35,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                       ___,      ___,        ___,      ___
     ),
     [PUN] = LAYOUT(
-        G(KC_Q),  G(KC_W),  G(KC_F),  G(KC_P),  TO(BASE),   KC_6,     KC_7,     KC_8,     KC_9,     KC_0,
+        G(KC_Q),  G(KC_W),  G(KC_F),  G(KC_P),  TO(BASE),   KC_0,     KC_2,     KC_4,     KC_6,     KC_8,
         OSM_LCTL, OSM_LOPT, OSM_LCMD, OSM_LSFT, TO(WIN),    EQL_PAD,  KC_COLN,  KC_COMM,  KC_SCLN,  KC_MINS,
         G(KC_Z),  G(KC_X),  G(KC_C),  TT(NAV),  G(KC_V),    KC_PIPE,  KC_SLSH,  KC_QUOT,  KC_TILD,  KC_BSLS,
                                       ___,      ___,        NEXTSEN,  MO(FUN)
     ),
     [SYM] = LAYOUT(
-        KC_1,     KC_2,     KC_3,     KC_4,     KC_5,       XXX,      XXX,      XXX,      XXX,      XXX,
+        KC_7,     KC_5,     KC_3,     KC_1,     KC_9,       XXX,      XXX,      XXX,      XXX,      XXX,
         KC_LT,    KC_LBRC,  KC_LPRN,  KC_LCBR,  KC_TAB,     XXX,      OSM_RSFT, OSM_RCMD, OSM_ROPT, OSM_RCTL,
         KC_GT,    KC_RBRC,  KC_RPRN,  KC_RCBR,  KC_GRV,     XXX,      KC_VOLD,  KC_MUTE,  KC_VOLU,  XXX,
                                       TT(NUM),  KC_BSPC,    ___,      ___
